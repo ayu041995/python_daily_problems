562588d984b508d255c1cf807459a292a728d170
Ayushi Jaiswal <ayu041995@gmail.com>
2024-09-03 09:27:03 +0530

    commit

---
+++ codeLearning/print.py
@@ -0,0 +1,19 @@
+print("Ayushi")
+
+# sep in print
+#Ayushi-Jaiswal
+
+print("Ayushi","Jaiswal", sep = '-')
+
+# end in print
+#Ayushi.Jaiswal
+
+print("Ayushi", end = '.')
+print("Jaiswal")
+
+# new line
+# Ayushi.
+# Jaiswal
+
+print("Ayushi", end = '.\n')
+print("Jaiswal")
