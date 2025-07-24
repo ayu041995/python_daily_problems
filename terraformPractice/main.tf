1f9e71733f9ac962db5d4f6158c7be6d4f967ed7
Ayushi Jaiswal <ayu041995@gmail.com>
2025-07-24 14:46:18 +0530

    my daily problems

--- terraformPractice/main.tf
+++ terraformPractice/main.tf
@@ -1,5 +1,8 @@
 # Simple example of terraform file creation
-
+provider "aws"{
+    region = "us-east-1"
+    profile = "GSC-prod-1"
+}
 
 resource "aws_instance" "webserver" {
     ami = "ami_0rfnsbdfvtg87fc7"
@@ -85,6 +88,51 @@ resource "aws_security_group" "example"{
         }
     }
 }
+
+# Use a variable to control the number of EC2 instances to create
+
+variable instances {
+    default {
+        count = 2
+    }
+}
+
+resource "aws_instance" "webservers" {
+    count = var.instances.count
+    ami = "ami_0rfnsbdfvtg87fc7"
+    instance_type = "m2.xlarge"
+    tags = {
+        Name = "Webserver-${count.index}"
+    }
+}
+
+
+# Create multiple S3 buckets using for_each and a list of names.
+
+variable "nameS3" {
+    default = ["dev","prod","stage"]
+}
+
+resource "aws_s3_bucket" "my_finance_bucket"{
+    for_each = toset(var.nameS3)
+    bucket = each.value
+}
+
+# output block to display the public IP of an EC2 instance
+
+output "ec2_public_ip" {
+    value = aws_instance.webserver.ec2_public_ip
+}
+
+# Import an existing EC2 instance into Terraform.
+
+resource "aws_instance" "existing"{
+
+}
+
+# terraform import aws_instance.existing i-0123456789abcdef0
+
+
 
 
 
